0c4a96ad277f61fb111eadd98d85653ecd3dce00
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2019-09-02 22:47:43 -0700

    standardized all array/obj names

--- data.js
+++ data.js
@@ -6,9 +6,9 @@ const npsSearchURL = "https://developer.nps.gov/api/v1/parks?api_";
 const weatherApiKey = "77e0f092ff8c489aa8fe507cb942e633";
 const weatherSearchURL = "https://api.weatherbit.io/v2.0/forecast/daily?";
 
-const reviewAPIKeyClient = "J2OYBRJYIRVZJCLVFDQZNXJUIUYT2JZYIYQ3OFJ0D2WQ0D0N";
-const reviewApiKey = "XGXG155FFGLLKP3PT4OEHFJNPPIMS0Z0VFDKQOBUQ24A25MU";
-const reviewSearchURL = "https://api.foursquare.com/v2/venues/explore?";
+const SuggestionAPIKeyClient = "J2OYBRJYIRVZJCLVFDQZNXJUIUYT2JZYIYQ3OFJ0D2WQ0D0N";
+const SuggestionApiKey = "XGXG155FFGLLKP3PT4OEHFJNPPIMS0Z0VFDKQOBUQ24A25MU";
+const SuggestionSearchURL = "https://api.foursquare.com/v2/venues/explore?";
 
 
 
@@ -17,13 +17,13 @@ function watchForm() {
     event.preventDefault();
     $("#js-error-message").addClass("hidden");
     const searchTerm = $("#js-search-term").val();
-    outdoorLife = [];
-    groceryStore = [];
-    nightLife = [];
-    answers = {};
+    finalOutdoorArr = [];
+    finalGroceryArr = [];
+    nightLifeObj= {};
+    results = {};
     arr = [];
-    forecastArr = [];
-    answersArr = [];
+    finalForecastArr = [];
+    resultsArr = [];
     $("#results-list").empty();
     getParks(searchTerm);
   });
@@ -34,8 +34,8 @@ function formatQueryParams(params) {
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join("&");
 }
-let answers = {};
-let answersArr = [];
+let results = {};
+let resultsArr = [];
 
 function watchReset() {
   $(".reset").click(event => {
@@ -60,59 +60,57 @@ function displayResults(responseJson) {
 
   }
   else {
-    let arrayItems=[]
+    let resultsCount=[]
     $("#results-list").empty();
     
     for (let i = 0; i < responseJson.data.length; i++) {
       arr.push(responseJson.data[i].name);
-      arrayItems.push(arr[i]);
+      resultsCount.push(arr[i]);
     }
     
-    $("#results-list").append(`<p>${arrayItems.length} Search Results</p>`);
-    for (let i=0; i<arrayItems.length; i++){
-      answers[i]={};
-      let weatherResults={};
-      answers[i].name = responseJson.data[i].name;
-      answers[i].description= responseJson.data[i].description;
-      answers[i].address = {};
-      answers[i].address.line1 = responseJson.data[i].addresses[1].line1;
-      answers[i].address.line2= responseJson.data[i].addresses[1].line2;
-      answers[i].address.line3 = responseJson.data[i].addresses[1].line3;
-      answers[i].address.city = responseJson.data[i].addresses[1].city;
-      answers[i].address.state= responseJson.data[i].addresses[1].stateCode;
-      answers[i].address.zip = responseJson.data[i].addresses[1].postalCode;
-      answers[i].url= responseJson.data[i].url; 
-      answers[i].weather={};
-      answers[i].entertainment={};
-      getWeather(answers[i].address.zip);
-      getReviews(answers[i].address.zip);
-      for(let j=0; j<forecastArr.length; j++){
-        if (forecastArr[j].zipCode===answers[i].address.zip){
-          answers[i].weather=forecastArr[j];
+    $("#results-list").append(`<p>${resultsCount.length} Search Results</p>`);
+    for (let i=0; i<resultsCount.length; i++){
+      results[i]={};
+      results[i].name = responseJson.data[i].name;
+      results[i].description= responseJson.data[i].description;
+      results[i].address = {};
+      results[i].address.line1 = responseJson.data[i].addresses[1].line1;
+      results[i].address.line2= responseJson.data[i].addresses[1].line2;
+      results[i].address.line3 = responseJson.data[i].addresses[1].line3;
+      results[i].address.city = responseJson.data[i].addresses[1].city;
+      results[i].address.state= responseJson.data[i].addresses[1].stateCode;
+      results[i].address.zip = responseJson.data[i].addresses[1].postalCode;
+      results[i].url= responseJson.data[i].url; 
+      results[i].weather={};
+      results[i].entertainment={};
+      getWeather(results[i].address.zip);
+      for(let j=0; j<finalForecastArr.length; j++){
+        if (finalForecastArr[j].zipCode===results[i].address.zip){
+          results[i].weather=finalForecastArr[j];
         }
       }
-      getReviews(answers[i].address.zip);
-      for(let j=0; j<nightList.length; j++){
-        if (nightList[j].zipCode===answers[i].address.zip){
-          answers[i].entertainment.nightLife=nightList[j].nightlife;
+      getSuggestions(results[i].address.zip);
+      for(let j=0; j<finalNightlifeArr.length; j++){
+        if (finalNightlifeArr[j].zipCode===results[i].address.zip){
+          results[i].entertainment.nightLife=finalNightlifeArr[j].nightlife;
         }
       }
-      for(let j=0; j<nightList.length; j++){
-        if (nightList[j].zipCode===answers[i].address.zip){
-          answers[i].entertainment.nightLife=nightList[j].nightlife;
+      for(let j=0; j<finalNightlifeArr.length; j++){
+        if (finalNightlifeArr[j].zipCode===results[i].address.zip){
+          results[i].entertainment.nightLife=finalNightlifeArr[j].nightlife;
         }
       }
-      for(let j=0; j<groceryStore.length; j++){
-        if (groceryStore[j].zipCode===answers[i].address.zip){
-          answers[i].entertainment.grocery=groceryStore[j].grocery;
+      for(let j=0; j<finalGroceryArr.length; j++){
+        if (finalGroceryArr[j].zipCode===results[i].address.zip){
+          results[i].entertainment.grocery=finalGroceryArr[j].grocery;
         }
       }
-      for(let j=0; j<outdoorLife.length; j++){
-        if (outdoorLife[j].zipCode===answers[i].address.zip){
-          answers[i].entertainment.outdoors=outdoorLife[j].outdoors;
+      for(let j=0; j<finalOutdoorArr.length; j++){
+        if (finalOutdoorArr[j].zipCode===results[i].address.zip){
+          results[i].entertainment.outdoors=finalOutdoorArr[j].outdoors;
         }
       }
-      $("#results-list").append(`<h2 class="parkName">${answers[i].name}</h2><p>${answers[i].description}</p><section class="grid-holder"><ul class="grid-hold" class="parkAddress">Address<li>${answers[i].address.line1}</li><li>${answers[i].address.line2}</li><li>${answers[i].address.line3}</li><li>${answers[i].address.city}, ${answers[i].address.state} ${answers[i].address.zip} </li></ul><ul class="grid-hold"><a href="${answers[i].url}" target="_blank"><li>Park Website</a></li></ul></section><p><img src="weather.png"></p><h2>Check out the forecast</h2><p class="bold">Forecast for ${answers[i].name}:</p><section class="grid-container"> <ul> <li class="grid-item">Today: ${answers[i].weather.today}&deg F</li><li class="grid-item"> ${answers[i].weather.description1}</li></ul><ul><li class="grid-item">Tomorrow: ${answers[i].weather.tomorrow}&deg F</li><li class="grid-item">${answers[i].weather.description2}</li></ul><ul><li class="grid-item">Next Day: ${answers[i].weather.nextDay}&deg F</li><li class="grid-item">${answers[i].weather.description3}</li></ul></section><p><img src="park.png"><h2>Check out the Nearby Attractions for ${answers[i].name}</h2></p><ul><h3>Night Life:</h3><li> ${answers[i].entertainment.nightLife}</li></ul><ul><h3>Grocery & Fast food:</h3> <li> ${answers[i].entertainment.grocery}</li></ul><ul><h3>Outdoor Recreation:</h3> <li> ${answers[i].entertainment.outdoors}</li></ul>`)
+      $("#results-list").append(`<h2 class="parkName">${results[i].name}</h2><p>${results[i].description}</p><section class="grid-holder"><ul class="grid-hold" class="parkAddress">Address<li>${results[i].address.line1}</li><li>${results[i].address.line2}</li><li>${results[i].address.line3}</li><li>${results[i].address.city}, ${results[i].address.state} ${results[i].address.zip} </li></ul><ul class="grid-hold"><a href="${results[i].url}" target="_blank"><li>Park Website</a></li></ul></section><p><img src="weather.png"></p><h2>Check out the forecast</h2><p class="bold">Forecast for ${results[i].name}:</p><section class="grid-container"> <ul> <li class="grid-item">Today: ${results[i].weather.today}&deg F</li><li class="grid-item"> ${results[i].weather.description1}</li></ul><ul><li class="grid-item">Tomorrow: ${results[i].weather.tomorrow}&deg F</li><li class="grid-item">${results[i].weather.description2}</li></ul><ul><li class="grid-item">Next Day: ${results[i].weather.nextDay}&deg F</li><li class="grid-item">${results[i].weather.description3}</li></ul></section><p><img src="park.png"><h2>Check out the Nearby Attractions for ${results[i].name}</h2></p><ul><h3>Night Life:</h3><li> ${results[i].entertainment.nightLife}</li></ul><ul><h3>Grocery & Fast food:</h3> <li> ${results[i].entertainment.grocery}</li></ul><ul><h3>Outdoor Recreation:</h3> <li> ${results[i].entertainment.outdoors}</li></ul>`)
     }    
 
 
@@ -182,7 +180,7 @@ function formatQueryParamsWeather(params) {
 }
 
 let forecast = {};
-let forecastArr = [];
+let finalForecastArr = [];
 function displayWeatherResults(responseJson,query) {
   forecast={}
   forecast = {
@@ -194,22 +192,21 @@ function displayWeatherResults(responseJson,query) {
     nextDay: responseJson.data[2].rh,
     description3: responseJson.data[2].weather.description
   };
-  forecastArr.push(forecast);
+  finalForecastArr.push(forecast);
 }
 
 
-// //per API restrictions, had to switch from reviews of parks to nearby attractions
 
-function getReviews(query) {
+function getSuggestions(query) {
   const params = {
     near: query,
-    client_id: reviewAPIKeyClient,
-    client_secret: reviewApiKey,
+    client_id: SuggestionAPIKeyClient,
+    client_secret: SuggestionApiKey,
     v: 20190811,
 
   };
-  const queryString = formatQueryParamsReviews(params)
-  const url = reviewSearchURL + queryString;
+  const queryString = formatQueryParamsSuggestions(params)
+  const url = SuggestionSearchURL + queryString;
 
  
 
@@ -220,36 +217,36 @@ function getReviews(query) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayReviewResults(responseJson,query))
+    .then(responseJson => displaySuggestionResults(responseJson,query))
     .catch(err => {
       $("#js-error-message").text(`Something went wrong: ${err.message}`)
     });
     
 }
 
-function formatQueryParamsReviews(params) {
+function formatQueryParamsSuggestions(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join("&");
 }
 
-let nightList = [];
-let grocery = {};
-let outdoors = {};
+let finalNightlifeArr = [];
+let groceryObj = {};
+let outdoorsObj = {};
 let nightLifeArr = [];
-let nightLife = {};
-let groceryStore = [];
+let nightLifeObj= {};
+let finalGroceryArr = [];
 let groceryArr = [];
 let outdoorsArr = [];
-let outdoorLife = [];
+let finalOutdoorArr = [];
 
-function displayReviewResults(responseJson,query) {
+function displaySuggestionResults(responseJson,query) {
   nightLifeArr=[];
-  nightLife={};
+  nightLifeObj={};
   groceryArr=[];
   grocery={};
   outdoorsArr=[];
-  outdoors={};
+  outdoorsObj={};
   for (let j = 0; j < responseJson.response.groups[0].items.length; j++) {
     if (`${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Sports Bar" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Bar" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Brewery") {
       nightLifeArr.push(` ${responseJson.response.groups[0].items[j].venue.name}`)
@@ -257,32 +254,32 @@ function displayReviewResults(responseJson,query) {
   
    
     if (nightLifeArr.length > 0) {
-      nightLife = { zipCode: query, nightlife: nightLifeArr };
+      nightLifeObj= { zipCode: query, nightlife: nightLifeArr };
     }
-    else nightLife = { zipCode: query, nightlife: "None" };
-    nightList.push(nightLife);
+    else nightLifeObj= { zipCode: query, nightlife: "None" };
+    finalNightlifeArr.push(nightLife);
     
     if (`${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Café" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Diner" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Coffee Shop" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Deli / Bodega" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Fast Food Restaurant" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Grocery Store" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Sandwich Place") {
       groceryArr.push(` ${responseJson.response.groups[0].items[j].venue.name}`)
     }
     if (groceryArr.length > 0) {
-      grocery = { zipCode: query, grocery: groceryArr };
+      groceryObj = { zipCode: query, grocery: groceryArr };
     }
-    else grocery = { zipCode: query, grocery: "None" };
+    else groceryObj = { zipCode: query, grocery: "None" };
 
-    groceryStore.push(grocery);
+    finalGroceryArr.push(groceryObj);
 
     if (`${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Lake" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Harbor Marina" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Bay" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Bike Trail" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Yoga Studio" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Campground" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "Botanical Garden" || `${responseJson.response.groups[0].items[j].venue.categories[0].name}` === "State / Provincial Park") {
       outdoorsArr.push(` ${responseJson.response.groups[0].items[j].venue.name}`)
     }
     if (outdoorsArr.length > 0) {
-      outdoors = { zipCode: query, outdoors: outdoorsArr };
+      outdoorsObj = { zipCode: query, outdoors: outdoorsArr };
     }
-    else outdoors = {zipCode: query,  outdoors: "None" };
+    else outdoorsObj = {zipCode: query,  outdoors: "None" };
   
 
 
-  outdoorLife.push(outdoors);
+  finalOutdoorArr.push(outdoors);
   }
 
 }
